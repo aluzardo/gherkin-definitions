a46089529d87452565544b3d5d06c66d66be66f7
Rick de Hoop <rickdehoop@gmail.com>
2015-06-11 10:49:25 +0200

    gulp task can be used multiple times now in a gulpfile

--- gulp.js
+++ gulp.js
@@ -1,8 +1,10 @@
 var expandDefinitions = require('./');
 var map = require('vinyl-map');
 
-module.exports = map(function mapFile(contents) {
-    var input = contents.toString();
-    var output = expandDefinitions(input);
-    return output;
-});
+module.exports = function streamWrapper() {
+    return map(function mapFile(contents) {
+        var input = contents.toString();
+        var output = expandDefinitions(input);
+        return output;
+    });
+};
